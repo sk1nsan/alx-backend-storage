9ebef74f2c27bb9f5a675381bca6f41230bf3120
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-17 21:31:04 +0300

    task 10

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -2,8 +2,8 @@
 -- creates a function SafeDiv that dividesthe first by the second number or returns 0 if the second number is equal to 0.
 DELIMITER $$
 DROP FUNCTION IF EXISTS SafeDiv;
-CREATE FUNCTION SafeDiv (IN a INT, IN b INT)
-RETURNS FLOAT DETERMINISTIC
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS DOUBLE DETERMINISTIC
 BEGIN
   IF b = 0 THEN
     RETURN 0;
