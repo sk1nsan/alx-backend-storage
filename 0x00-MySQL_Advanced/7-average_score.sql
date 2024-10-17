c105776337cfeb3e542fce74c83e7a65b5e8736c
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-17 20:36:21 +0300

    task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,7 +4,7 @@ DELIMITER $$ ;
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
   DECLARE average_score DECIMAL;
-  SELECT AVERAGE(score) INTO average_score FROM corrections WHERE user_id=user_id;
+  SELECT AVERAGE(score) INTO average_score FROM corrections WHERE corrections.user_id=user_id;
   UPDATE users SET average_score = average_score WHERE id=user_id;
 END$$
 DELIMITER ; $$
