1ee801186ddc29b6f4aeefa2f37d7f23ada77132
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-17 20:19:41 +0300

    task 7

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,10 @@
+-- Average score 
+-- reates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
+DELIMITER $$ ;
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+  DECLARE average_score DECIMAL;
+  SELECT AVERAGE(score) INTO average_score FROM corrections WHERE user_id=user_id;
+  UPDATE users SET average_score = average_score WHERE id=user_id;
+END$$
+DELIMITER ; $$
