c7bb426283a237a1dcc99b7b1a9279df42a58b5e
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-10-23 20:12:23 +0300

    task 1 and 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,22 @@
 
 import redis
 from uuid import uuid4
+from typing import Callable, TypeVar, Any
+from functools import wraps
+
+T = TypeVar("T")
+
+
+def count_calls(method: Callable[..., Any]) -> Callable[..., Any]:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        name = method.__qualname__
+        if (self._redis.get(name)):
+            self._redis.incr(name)
+        else:
+            self._redis.set(name, 1)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -10,7 +26,26 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
+    @count_calls
+    def store(self, data: str | bytes | int | float) -> str:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: None | Callable[[bytes], T] = None) -> T:
+        result = self._redis.get(key)
+        if not result or not fn:
+            return result
+        return fn(result)
+
+    def get_str(self, key: str) -> str:
+        result = self._redis.get(key)
+        if not result:
+            return result
+        return str(result)
+
+    def get_int(self, key: str) -> int:
+        result = self._redis.get(key)
+        if not result:
+            return result
+        return int(result)
